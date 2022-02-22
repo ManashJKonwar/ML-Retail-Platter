797d806a7ade6f7f290bee01d561950e6bcf956d
ManashJKonwar <rickykonwar@gmail.com>
2022-02-22 23:22:19 +0530

    Enhancement: Entry_Script_to_the_retail_pricing_simulator
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

---
+++ index.py
@@ -0,0 +1,22 @@
+__author__ = "konwar.m"
+__copyright__ = "Copyright 2021, AI R&D"
+__credits__ = ["konwar.m"]
+__license__ = "Individual Ownership"
+__version__ = "1.0.1"
+__maintainer__ = "konwar.m"
+__email__ = "rickykonwar@gmail.com"
+__status__ = "Development"
+
+import os
+from app import app
+from layouts import layout_sidepanel
+import callbacks
+
+app.title = "Retail Pricing Simulator"
+app.layout = layout_sidepanel.layout
+
+if __name__ == '__main__':
+    if os.environ["ENV"] == "deployment":
+        app.run_server(host="0.0.0.0", port=8050)
+    else:
+        app.run_server(debug=True, port=8052)
