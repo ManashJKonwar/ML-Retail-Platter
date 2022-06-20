aa04c2b730637a1f58a6b3460b3efe861c27dfaa
ManashJKonwar <rickykonwar@gmail.com>
2022-06-20 17:32:28 +0530

    Update: Setting_up_dropdown_options

--- callbacks/callbacks_sidepanel.py
+++ callbacks/callbacks_sidepanel.py
@@ -7,9 +7,11 @@ __maintainer__ = "konwar.m"
 __email__ = "rickykonwar@gmail.com"
 __status__ = "Development"
 
+from dash import no_update
 from dash.dependencies import Input, Output, State
 from callback_manager import CallbackManager
 from layouts import layout_retail_summary
+from datasets.backend import df_transactions, df_products, df_shops
 
 callback_manager = CallbackManager()
 
@@ -18,3 +20,21 @@ callback_manager = CallbackManager()
 def render_content(tab):
     if tab == 'tab-1':
         return layout_retail_summary.layout
+
+@callback_manager.callback([Output(component_id='dd-product-name', component_property='options'),
+                        Output(component_id='dd-product-name', component_property='value')],
+                        Input(component_id='dd-shop-name', component_property='value'))
+def set_product_options(sel_shop_names):
+    if isinstance(sel_shop_names, list):
+        sel_df_shop_names = df_shops.loc[df_shops.translated_shop_name.isin(sel_shop_names)].reset_index(drop=True)
+
+        # Extracting transactions for selected shop names
+        option_mask = (df_transactions.shop_id.isin(list(sel_df_shop_names.shop_id.unique())))
+        sel_df_transactions = df_transactions.loc[option_mask].reset_index(drop=True)
+
+        # Extracting final product options and setting default as first product
+        final_options = sorted(list(df_products.loc[df_products.item_id.isin(list(sel_df_transactions.item_id.unique()))].translated_item_name.unique()))
+
+        return final_options, [final_options[0]]
+    else:
+        return no_update     
