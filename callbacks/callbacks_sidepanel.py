83b4efcc2d9145db416ecb55dbf8f3cd148d2630
ManashJKonwar <rickykonwar@gmail.com>
2022-06-20 20:39:06 +0530

    Update: Setting_up_shop_name_options_as_callback

--- callbacks/callbacks_sidepanel.py
+++ callbacks/callbacks_sidepanel.py
@@ -11,7 +11,7 @@ from dash import no_update
 from dash.dependencies import Input, Output, State
 from callback_manager import CallbackManager
 from layouts import layout_retail_summary
-from datasets.backend import df_transactions, df_products, df_shops
+from datasets.backend import df_transactions, df_products, df_shops, df_product_categories
 
 callback_manager = CallbackManager()
 
@@ -23,18 +23,58 @@ def render_content(tab):
 
 @callback_manager.callback([Output(component_id='dd-product-name', component_property='options'),
                         Output(component_id='dd-product-name', component_property='value')],
-                        Input(component_id='dd-shop-name', component_property='value'))
-def set_product_options(sel_shop_names):
-    if isinstance(sel_shop_names, list):
-        sel_df_shop_names = df_shops.loc[df_shops.translated_shop_name.isin(sel_shop_names)].reset_index(drop=True)
+                        Input(component_id='dd-product-category', component_property='value'))
+def set_product_options(sel_product_categories):
+    if isinstance(sel_product_categories, list):
+        sel_df_product_categories = df_product_categories.loc[df_product_categories.translated_item_category_name.isin(sel_product_categories)].reset_index(drop=True)
 
-        # Extracting transactions for selected shop names
-        option_mask = (df_transactions.shop_id.isin(list(sel_df_shop_names.shop_id.unique())))
-        sel_df_transactions = df_transactions.loc[option_mask].reset_index(drop=True)
+        # Extracting product names based on product categories
+        product_mask = df_products.item_category_id.isin(list(sel_df_product_categories.item_category_id.unique()))
+        sel_df_product = df_products.loc[product_mask].reset_index(drop=True)
 
         # Extracting final product options and setting default as first product
-        final_options = sorted(list(df_products.loc[df_products.item_id.isin(list(sel_df_transactions.item_id.unique()))].translated_item_name.unique()))
+        final_options = sorted(list(sel_df_product.translated_item_name.unique()))
 
         return final_options, [final_options[0]]
     else:
-        return no_update     
+        return no_update 
+
+@callback_manager.callback([Output(component_id='dd-shop-name', component_property='options'),
+                        Output(component_id='dd-shop-name', component_property='value')],
+                        Input(component_id='dd-product-name', component_property='value'))
+def set_product_options(sel_product_names):
+    if isinstance(sel_product_names, list):
+        sel_df_product = df_products.loc[df_products.translated_item_name.isin(sel_product_names)].reset_index(drop=True)
+
+        # Extracting shop ids based on product names
+        shop_mask = df_transactions.item_id.isin(list(sel_df_product.item_id.unique()))
+        sel_df_transactions = df_transactions.loc[shop_mask].reset_index(drop=True)
+
+        # Extracting shop names based on extracted shop ids
+        shop_id_list = sorted(list(sel_df_transactions.shop_id.unique()))
+        sel_df_shop = df_shops.loc[df_shops.shop_id.isin(shop_id_list)].reset_index(drop=True)
+
+        # Extracting final shop optons and setting default as first shop
+        final_options = sorted(list(sel_df_shop.translated_shop_name.unique()))
+
+        return final_options, [final_options[0]]
+    else:
+        return no_update 
+
+# @callback_manager.callback([Output(component_id='dd-product-name', component_property='options'),
+#                         Output(component_id='dd-product-name', component_property='value')],
+#                         Input(component_id='dd-shop-name', component_property='value'))
+# def set_product_options(sel_shop_names):
+#     if isinstance(sel_shop_names, list):
+#         sel_df_shop_names = df_shops.loc[df_shops.translated_shop_name.isin(sel_shop_names)].reset_index(drop=True)
+
+#         # Extracting transactions for selected shop names
+#         option_mask = (df_transactions.shop_id.isin(list(sel_df_shop_names.shop_id.unique())))
+#         sel_df_transactions = df_transactions.loc[option_mask].reset_index(drop=True)
+
+#         # Extracting final product options and setting default as first product
+#         final_options = sorted(list(df_products.loc[df_products.item_id.isin(list(sel_df_transactions.item_id.unique()))].translated_item_name.unique()))
+
+#         return final_options, [final_options[0]]
+#     else:
+#         return no_update     
