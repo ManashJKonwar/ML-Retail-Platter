835cd1fe3f54bec35fa23ca72bd7ff6fd09a8324
ManashJKonwar <rickykonwar@gmail.com>
2022-06-26 00:08:54 +0530

    Enhancement: Adding_spinner_to_retail_tab_components

--- layouts/layout_retail_summary.py
+++ layouts/layout_retail_summary.py
@@ -11,85 +11,95 @@ import dash_bootstrap_components as dbc
 from dash import dcc
 from dash import html
 
-sales_card = dbc.Card([
-                dbc.CardImg(
-                    src="/assets/retail_tab_images/revenue_card.jpg", 
-                    top=True,
-                    style={'opacity':0.3, 'width':'100%', 'height':'10vw'}
-                ),
-                dbc.CardImgOverlay(
-                    dbc.CardBody([
-                        html.H5("Sales Card", className="card-title"),
-                        html.P(
-                            "Total Sales: %s ₽" %(str(500)),
-                            id='p-salestext',
-                            className="card-text",
-                        ),
-                    ])
-                )
-            ], color="secondary", inverse=True)
-
-category_card = dbc.Card([
+sales_card = dbc.Spinner(
+                dbc.Card([
                     dbc.CardImg(
-                        src="/assets/retail_tab_images/category_card.jpg", 
+                        src="/assets/retail_tab_images/revenue_card.jpg", 
                         top=True,
                         style={'opacity':0.3, 'width':'100%', 'height':'10vw'}
                     ),
                     dbc.CardImgOverlay(
                         dbc.CardBody([
-                            html.H5("Category Card", className="card-title"),
-                            html.P([
-                                "Best Category: %s" %('Accessories'),
-                                html.Br(),
-                                "Total no of products sold within this category: %s" %(str(50))],
-                                id='p-categorytext',
+                            html.H5("Sales Card", className="card-title"),
+                            html.P(
+                                "Total Sales: %s ₽" %(str(500)),
+                                id='p-salestext',
                                 className="card-text",
                             ),
                         ])
                     )
                 ], color="secondary", inverse=True)
+            )
 
-product_card = dbc.Card([
+category_card = dbc.Spinner(
+                    dbc.Card([
+                        dbc.CardImg(
+                            src="/assets/retail_tab_images/category_card.jpg", 
+                            top=True,
+                            style={'opacity':0.3, 'width':'100%', 'height':'10vw'}
+                        ),
+                        dbc.CardImgOverlay(
+                            dbc.CardBody([
+                                html.H5("Category Card", className="card-title"),
+                                html.P([
+                                    "Best Category: %s" %('Accessories'),
+                                    html.Br(),
+                                    "Total no of products sold within this category: %s" %(str(50))],
+                                    id='p-categorytext',
+                                    className="card-text",
+                                ),
+                            ])
+                        )
+                    ], color="secondary", inverse=True)
+                )
+
+product_card = dbc.Spinner(
+                    dbc.Card([
+                        dbc.CardImg(
+                            src="/assets/retail_tab_images/product_card.jpg", 
+                            top=True,
+                            style={'opacity':0.3, 'width':'100%', 'height':'10vw'}
+                        ),
+                        dbc.CardImgOverlay(
+                            dbc.CardBody([
+                                html.H5("Product Card", className="card-title"),
+                                html.P([
+                                    "Best Product: %s" %('Ps4'),
+                                    html.Br(),
+                                    "Total sales for this product: %s ₽" %(str(50))],
+                                    id='p-producttext',
+                                    className="card-text",
+                                ),
+                            ])
+                        )
+                    ], color="secondary", inverse=True)
+                )
+
+shop_card = dbc.Spinner(
+                dbc.Card([
                     dbc.CardImg(
-                        src="/assets/retail_tab_images/product_card.jpg", 
-                        top=True,
-                        style={'opacity':0.3, 'width':'100%', 'height':'10vw'}
+                            src="/assets/retail_tab_images/shop_card.jpg", 
+                            top=True,
+                            style={'opacity':0.3, 'width':'100%', 'height':'10vw'}
                     ),
                     dbc.CardImgOverlay(
                         dbc.CardBody([
-                            html.H5("Product Card", className="card-title"),
+                            html.H5("Shopping Card", className="card-title"),
                             html.P([
-                                "Best Product: %s" %('Ps4'),
+                                "Best Store: %s" %('Fancy Store'),
                                 html.Br(),
-                                "Total sales for this product: %s ₽" %(str(50))],
-                                id='p-producttext',
+                                "Total sales from this shop: %s ₽" %(str(200))],
+                                id='p-shoptext',
                                 className="card-text",
                             ),
                         ])
                     )
                 ], color="secondary", inverse=True)
+            )
 
-shop_card = dbc.Card([
-                dbc.CardImg(
-                        src="/assets/retail_tab_images/shop_card.jpg", 
-                        top=True,
-                        style={'opacity':0.3, 'width':'100%', 'height':'10vw'}
-                ),
-                dbc.CardImgOverlay(
-                    dbc.CardBody([
-                        html.H5("Shopping Card", className="card-title"),
-                        html.P([
-                            "Best Store: %s" %('Fancy Store'),
-                            html.Br(),
-                            "Total sales from this shop: %s ₽" %(str(200))],
-                            id='p-shoptext',
-                            className="card-text",
-                        ),
-                    ])
+category_graph = dbc.Spinner(
+                    dcc.Graph(id='g-category', style={'width': '100%', 'height': '70vh'})
                 )
-            ], color="secondary", inverse=True)
-
-category_graph = dcc.Graph(id='g-category', style={'width': '100%', 'height': '70vh'})
 
 layout = html.Div([
             dbc.Row([
