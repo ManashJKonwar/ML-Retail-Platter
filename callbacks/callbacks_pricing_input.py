f733c7fecea2484f7f98ac872fb057a8ef5ef2dd
ManashJKonwar <rickykonwar@gmail.com>
2022-06-28 16:11:08 +0530

    Enhancement: Setting_up_pricing_input_callback_for_table_population

--- callbacks/callbacks_pricing_input.py
+++ callbacks/callbacks_pricing_input.py
@@ -7,6 +7,7 @@ __maintainer__ = "konwar.m"
 __email__ = "rickykonwar@gmail.com"
 __status__ = "Development"
 
+import pandas as pd
 from dash.dependencies import Input, Output, State
 from callback_manager import CallbackManager
 from datasets.backend import df_consolidated, lt_month_range, lt_month2week_list, st_month_range, st_month2week_list
@@ -28,14 +29,39 @@ callback_manager = CallbackManager()
 def set_simulation_input_data(period_type, custom_start_date, custom_end_date, sel_product_category, sel_product, sel_shop, \
                                 uploaded_content, n_reset_simulator, stored_pricing_input):
     # Generate Consolidated DF based on Category, Product and Shop selected
-    print('df consolidated : ', df_consolidated.columns.unique())
     if period_type.__eq__('Quarterly'):
-        RSP_structure = pd.concat([df_consolidated[['PROVINCE','MANUF','BRAND','KA','PRICE_PACK_13W','PRICE_PACK_4W','PRICE_PACK_LATEST']],pd.DataFrame(columns=st_month_range)])
-        TMP_structure = pd.concat([df_consolidated[['PROVINCE','MANUF','BRAND','KA','PRICE_PACK_13W','PRICE_PACK_4W','PRICE_PACK_LATEST']],pd.DataFrame(columns=st_month2week_list)])
+        RSP_structure = pd.concat([df_consolidated[['PRODUCT_CATEGORY','PRODUCT','SHOP','PRICE_PER_ITEM']],pd.DataFrame(columns=st_month_range)])
+        TMP_structure = pd.concat([df_consolidated[['PRODUCT_CATEGORY','PRODUCT','SHOP','PRICE_PER_ITEM']],pd.DataFrame(columns=st_month2week_list)])
     elif period_type.__eq__('Annually'):
-        RSP_structure = pd.concat([df_consolidated[['PROVINCE','MANUF','BRAND','KA','PRICE_PACK_13W','PRICE_PACK_4W','PRICE_PACK_LATEST']],pd.DataFrame(columns=lt_month_range)])
-        TMP_structure = pd.concat([df_consolidated[['PROVINCE','MANUF','BRAND','KA','PRICE_PACK_13W','PRICE_PACK_4W','PRICE_PACK_LATEST']],pd.DataFrame(columns=lt_month2week_list)])
+        RSP_structure = pd.concat([df_consolidated[['PRODUCT_CATEGORY','PRODUCT','SHOP','PRICE_PER_ITEM']],pd.DataFrame(columns=lt_month_range)])
+        TMP_structure = pd.concat([df_consolidated[['PRODUCT_CATEGORY','PRODUCT','SHOP','PRICE_PER_ITEM']],pd.DataFrame(columns=lt_month2week_list)])
     elif period_type.__eq__('Custom'):
         cdr_month_range, cdr_week_range = custom_datepicker(start_date=custom_start_date, end_date=custom_end_date)
-        RSP_structure = pd.concat([df_consolidated[['PROVINCE','MANUF','BRAND','KA','PRICE_PACK_13W','PRICE_PACK_4W','PRICE_PACK_LATEST']],pd.DataFrame(columns=cdr_month_range)])
-        TMP_structure = pd.concat([df_consolidated[['PROVINCE','MANUF','BRAND','KA','PRICE_PACK_13W','PRICE_PACK_4W','PRICE_PACK_LATEST']],pd.DataFrame(columns=cdr_week_range)])
+        RSP_structure = pd.concat([df_consolidated[['PRODUCT_CATEGORY','PRODUCT','SHOP','PRICE_PER_ITEM']],pd.DataFrame(columns=cdr_month_range)])
+        TMP_structure = pd.concat([df_consolidated[['PRODUCT_CATEGORY','PRODUCT','SHOP','PRICE_PER_ITEM']],pd.DataFrame(columns=cdr_week_range)])
+
+    # Table structure for RSP and TMP with default price across period rounded off to 3 decimals
+    RSP_structure['PRICE_PER_ITEM'] = RSP_structure['PRICE_PER_ITEM'].map("{:.2f}".format)
+    TMP_structure['PRICE_PER_ITEM'] = TMP_structure['PRICE_PER_ITEM'].map("{:.2f}".format)
+    RSP_default = RSP_structure.ffill(axis = 1)
+    TMP_default = TMP_structure.ffill(axis = 1)
+    
+    # Default structure
+    if period_type.__eq__('Quarterly'):
+        temp_default = TMP_default
+    elif period_type.__eq__('Annually'):
+        temp_default = RSP_default
+    elif period_type.__eq__('Custom'):
+        temp_default = TMP_default
+    df_templatedata = temp_default.loc[temp_default.PRODUCT_CATEGORY.isin(sel_product_category)  & temp_default.PRODUCT.isin(sel_product) & temp_default.SHOP.isin(sel_shop), :]
+
+    # Sorting Table with respect to combination of Province, Manuf and Brand
+    df_templatedata = df_templatedata.sort_values(by=['PRODUCT_CATEGORY','PRODUCT','SHOP']).reset_index(drop=True)
+
+    column_list = []
+    for column_name in df_templatedata.columns:
+        if column_name in ['PRODUCT_CATEGORY','PRODUCT','SHOP','PRICE_PER_ITEM']:
+            column_list.append({"name": column_name, "id": column_name, 'editable': False})
+        else:
+            column_list.append({"name": column_name, "id": column_name})
+    return df_templatedata.to_dict('records'), column_list
