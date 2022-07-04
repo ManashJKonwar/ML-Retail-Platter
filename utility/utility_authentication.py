c9e9586afdb166c78089fa04e818a8a40b6780a2
ManashJKonwar <rickykonwar@gmail.com>
2022-07-04 23:20:11 +0530

    Enhancement: Adding_authetication_utility_script
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

---
+++ utility/utility_authentication.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+#region Model Class
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True, nullable = False)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+#endregion
