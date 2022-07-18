b1d7ab5b7ac9d25f402f40e80390e4282536659d
ManashJKonwar <rickykonwar@gmail.com>
2022-07-18 16:56:20 +0530

    Update: Integrating_pivotted_callback_with_app
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ from callbacks.callbacks_sidepanel import callback_manager as sidepanel_callback
 from callbacks.callbacks_retail_summary import callback_manager as retail_summary_callback_manager
 from callbacks.callbacks_pricing_input import callback_manager as pricing_input_callback_manager
 from callbacks.callbacks_pricing_sales import callback_manager as prediction_output_callback_manager
+from callbacks.callbacks_pivot_kpis import callback_manager as pivotted_kpi_callback_manager
 from utility.utility_authentication import User, create_users_table
 
 # SQL Alchemy DB instance to use it under models
@@ -75,6 +76,7 @@ sidepanel_callback_manager.attach_to_app(app)
 retail_summary_callback_manager.attach_to_app(app)
 pricing_input_callback_manager.attach_to_app(app)
 prediction_output_callback_manager.attach_to_app(app)
+pivotted_kpi_callback_manager.attach_to_app(app)
 
 # Adding log folder if it not exists
 if not os.path.exists('logs'):
