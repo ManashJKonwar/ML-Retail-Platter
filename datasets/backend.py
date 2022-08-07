fe667cf07ac568fb623c7a10682ac6b55955d97d
ManashJKonwar <rickykonwar@gmail.com>
2022-08-07 18:57:05 +0530

    Enhancement: Adding_filter_for_uniques
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

--- datasets/backend.py
+++ datasets/backend.py
@@ -7,11 +7,14 @@ __maintainer__ = "konwar.m"
 __email__ = "rickykonwar@gmail.com"
 __status__ = "Development"
 
+import os
 import pickle
 import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
 from utility.utility_data_transformation import long_term_structure, short_term_structure, get_custom_dates
+
+model_version_flag = 'model_v02'
 
 #region Reading Main Dataframes and Adding Parent Categories
 df_product_categories = pd.read_csv(r'datasets\translated_item_categories.csv')
@@ -29,6 +32,11 @@ shop_names = sorted(list(df_shops.translated_shop_name.unique()))
 #endregion
 
 #region Data Preprocessing
+# Reading uniques 
+unique_dict = None
+with open(os.path.join('datasets','models_essentials_v02','uniques.pkl'),'rb') as unique_file:
+    unique_dict = pickle.load(unique_file)
+
 # Generating date month map
 df_date_week_map = df_transactions[['date', 'date_block_num']].copy().drop_duplicates().reset_index(drop=True)
 df_date_week_map['date'] = pd.to_datetime(df_date_week_map['date'], infer_datetime_format=True, format='%d.%m.%Y')
@@ -48,6 +56,13 @@ df_consolidated = pd.merge(df_consolidated, df_products[['item_id','item_categor
 df_consolidated = pd.merge(df_consolidated, df_product_categories[['item_category_id','translated_item_category_name', 'parent_category_name']], how='left', on='item_category_id').reset_index(drop=True)
 df_consolidated = pd.merge(df_consolidated, df_shops[['shop_id','translated_shop_name']], how='left', on='shop_id').reset_index(drop=True)
 
+# Restricting consolidated to unique ids used for training
+unique_mask = (df_consolidated.parent_category_name.isin(unique_dict['parent_category_names'])) & \
+            (df_consolidated.item_id.isin(unique_dict['item_id'])) & \
+            (df_consolidated.item_category_id.isin(unique_dict['item_category_id'])) & \
+            (df_consolidated.shop_id.isin(unique_dict['shop_id']))
+df_consolidated = df_consolidated.loc[unique_mask].reset_index(drop=True)
+
 # Renaming columns properly
 df_consolidated.rename(columns={'translated_item_name':'PRODUCT', 'translated_shop_name':'SHOP', 'translated_item_category_name':'PRODUCT_CATEGORY', \
                     'item_price':'PRICE_PER_ITEM', 'parent_category_name':'PARENT_CATEGORY'}, inplace=True)
@@ -62,49 +77,59 @@ custom_start_date, custom_end_date = get_custom_dates()
 
 #region Inferencing pipeline related
 derived_features_list = []
-with open(r'datasets\new_features.pkl', 'rb') as feature_file:
-    derived_features_list = pickle.load(feature_file)
+if model_version_flag.__eq__('model_v01'):
+    with open(r'datasets\new_features.pkl', 'rb') as feature_file:
+        derived_features_list = pickle.load(feature_file)
 
-def generate_features_df(features_list=[]):
-    df_features = pd.DataFrame(columns=['feature_name','feature_type','price_dependency']) 
-    try:
-        # Form a dataframe which maps each feature to type of it and whether it is 
-        # dependent on price change or not
-        for feature in features_list:
-            feature_type, price_dependency = '', False
-            # Segregating features
-            feature_splitted_list = feature.split('_')
-            if 'sales' in feature_splitted_list or 'cnt' in feature_splitted_list:
-                feature_type = 'sales'
-            elif 'price' in feature_splitted_list:
-                feature_type = 'price'
-                price_dependency = True
-            elif 'tfidf' in feature_splitted_list:
-                feature_type = 'text'
-            else:
-                if 'date' in feature_splitted_list:
-                    feature_type = 'month'
+    def generate_features_df(features_list=[]):
+        df_features = pd.DataFrame(columns=['feature_name','feature_type','price_dependency']) 
+        try:
+            # Form a dataframe which maps each feature to type of it and whether it is 
+            # dependent on price change or not
+            for feature in features_list:
+                feature_type, price_dependency = '', False
+                # Segregating features
+                feature_splitted_list = feature.split('_')
+                if 'sales' in feature_splitted_list or 'cnt' in feature_splitted_list:
+                    feature_type = 'sales'
+                elif 'price' in feature_splitted_list:
+                    feature_type = 'price'
+                    price_dependency = True
+                elif 'tfidf' in feature_splitted_list:
+                    feature_type = 'text'
                 else:
-                    feature_type = 'id'
+                    if 'date' in feature_splitted_list:
+                        feature_type = 'month'
+                    else:
+                        feature_type = 'id'
 
-            intermediate_features = pd.DataFrame.from_records([{'feature_name': feature, 'feature_type': feature_type, 'price_dependency': price_dependency}])
-            df_features = pd.concat([df_features, intermediate_features], ignore_index=True)
-        
-        return df_features
-    except Exception as ex:
-        print('Caught Exception while generating features dataframe as: %s' %(str(ex)))
-        pass
+                intermediate_features = pd.DataFrame.from_records([{'feature_name': feature, 'feature_type': feature_type, 'price_dependency': price_dependency}])
+                df_features = pd.concat([df_features, intermediate_features], ignore_index=True)
+            
+            return df_features
+        except Exception as ex:
+            print('Caught Exception while generating features dataframe as: %s' %(str(ex)))
+            pass
 
-df_features = generate_features_df(features_list=['shop_id', 'item_id', 'item_category_id', 'date_block_num', 'item_price'] + derived_features_list)
-df_variable_type = df_features.copy().reset_index(drop=True)
-df_features['MODEL_ID'] = 'MODEL_01'
-df_features = df_features.reset_index(drop=True)
+    df_features = generate_features_df(features_list=['shop_id', 'item_id', 'item_category_id', 'date_block_num', 'item_price'] + derived_features_list)
+    df_variable_type = df_features.copy().reset_index(drop=True)
+    df_seasonality = pd.DataFrame()
+    df_features['MODEL_ID'] = 'MODEL_01'
+    df_features = df_features.reset_index(drop=True)
+
+elif model_version_flag.__eq__('model_v02'):
+    df_features = pd.read_csv(os.path.join('datasets','models_essentials_v02','df_features.csv')) 
+    df_variable_type = df_features.copy().reset_index(drop=True)
+    df_seasonality = pd.read_csv(os.path.join('datasets','models_essentials_v02','df_seasonality.csv'))
 
 # Extract the xvars for last training month i.e. date_block_num == 33
-df_xvar = pd.read_csv('datasets\latest_dataset.csv')
+df_xvar = pd.read_csv(os.path.join('datasets','models_essentials_v01','df_xvar.csv')) if model_version_flag.__eq__('model_v01') else pd.read_csv(os.path.join('datasets','models_essentials_v02','df_xvar.csv'))
 
 # Extracting the maps
-dict_product_category_id_map = {row.item_category_name: row.item_category_id for row in pd.read_csv('datasets\item_categories_map.csv').itertuples()}
+if model_version_flag.__eq__('model_v02'):
+    with open(os.path.join('datasets','models_essentials_v02','parent_cat_map.pkl'), 'rb') as p_cat:
+        dict_parent_category_id_map = pickle.load(p_cat)
+dict_product_category_id_map = {row.item_category_name: row.item_category_id for row in pd.read_csv(os.path.join('datasets','models_essentials_v01','item_categories_training.csv')).itertuples()}
 dict_product_id_map = {row.translated_item_name: row.item_id for row in df_products[['item_id','translated_item_name']].drop_duplicates().itertuples()}
 dict_shop_id_map = {row.translated_shop_name: row.shop_id for row in df_shops[['shop_id','translated_shop_name']].drop_duplicates().itertuples()}
 #endregion
