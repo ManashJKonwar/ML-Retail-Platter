6b8224654c67b16ea54827966f17e6381ab1b0cb
ManashJKonwar <rickykonwar@gmail.com>
2022-09-06 21:35:32 +0530

    Enhancement: Adding_celery_logger_for_long_running_tasks
    Signed-off-by: ManashJKonwar <rickykonwar@gmail.com>

---
+++ config/celerytasklogger.py
@@ -0,0 +1,38 @@
+__author__ = "konwar.m"
+__copyright__ = "Copyright 2022, AI R&D"
+__credits__ = ["konwar.m"]
+__license__ = "Individual Ownership"
+__version__ = "1.0.1"
+__maintainer__ = "konwar.m"
+__email__ = "rickykonwar@gmail.com"
+__status__ = "Development"
+
+import logging
+
+class TaskFormatter(logging.Formatter):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        try:
+            from celery._state import get_current_task
+            self.get_current_task = get_current_task
+        except ImportError:
+            self.get_current_task = lambda: None
+
+    def format(self, record):
+        task = self.get_current_task()
+        if task and task.request:
+            record.__dict__.update(task_id=task.request.id,
+                                   task_name=task.name)
+        else:
+            record.__dict__.setdefault('task_name', '')
+            record.__dict__.setdefault('task_id', '')
+        return super().format(record)
+
+logger = logging.getLogger()
+sh = logging.StreamHandler()
+sh.setFormatter(
+    TaskFormatter(
+        '%(asctime)s - %(task_id)s - %(task_name)s - %(name)s - %(levelname)s - %(message)s'))
+logger.setLevel(logging.INFO)
+logger.addHandler(sh)
